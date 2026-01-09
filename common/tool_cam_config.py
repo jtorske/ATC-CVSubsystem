5a79fc0e6cd8661820154e49dfd140f02437448b
Jordan Torske <jordan.torske@ucalgary.ca>
2026-01-09 14:52:52 -0700

    improved calibrations for webcam and tool->cam down to <10cm in error

--- common/tool_cam_config.py
+++ common/tool_cam_config.py
@@ -1,13 +1,21 @@
 import numpy as np
 
+# Rotation from tool frame to camera frame
+# Best so far: flip X gave X=0.009, Y=0.003, Z=0.097
+# Try: flip X and flip Y (180 rotation around Z)
 R_tool_cam = np.array([
-    [0.0427410659228677, -0.0138602339960694, -0.9989900375866374],
-    [-0.0014141872016995, 0.9999019255393558, -0.0139333907304394],
-    [0.9990851822333948, 0.0020082868975191, 0.0427172731503502],
+    [-1.0,  0.0, 0.0],
+    [0.0,  -1.0, 0.0],
+    [0.0,   0.0, 1.0],
 ])
 
+# Translation from tool flange to camera lens (in meters)
+# MEASURE THESE ON YOUR ROBOT:
+#   X: left(-) / right(+) offset
+#   Y: backward(-) / forward(+) offset  
+#   Z: down(-) / up(+) offset
 t_tool_cam = np.array([
-    0.9121252450188331,
-    0.1491951002144791,
-    9.0383562020310961,
+    0.0,    # X: camera centered
+    0.006,  # Y: camera 0.6cm forward of flange
+    0.0,    # Z: no vertical offset
 ])
